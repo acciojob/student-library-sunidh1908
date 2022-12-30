1fda9d91c4122e2bebce532d7c08c829a6ffa9bd
sunidh1908 <sunidh.kumar@gmail.com>
2022-12-30 13:23:47 +0530

    student service

--- src/main/java/com/example/library/studentlibrary/services/StudentService.java
+++ src/main/java/com/example/library/studentlibrary/services/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.library.studentlibrary.services;
 
 import com.example.library.studentlibrary.models.Card;
+import com.example.library.studentlibrary.models.CardStatus;
 import com.example.library.studentlibrary.models.Student;
 import com.example.library.studentlibrary.repositories.StudentRepository;
 import org.slf4j.Logger;
@@ -19,26 +20,27 @@ public class StudentService {
     StudentRepository studentRepository4;
 
     public Student getDetailsByEmail(String email){
-        Student student = null;
-
+        Student student = studentRepository4.findByEmailId(email);
         return student;
     }
 
     public Student getDetailsById(int id){
-        Student student = null;
-
+        Student student = studentRepository4.findById(id).get();
         return student;
     }
 
     public void createStudent(Student student){
-
+        Student.builder().id(student.getId()).emailId(student.getEmailId()).name(student.getName()).
+                age(student.getAge()).country(student.getCountry()).createdOn(student.getCreatedOn()).
+                updatedOn(student.getUpdatedOn()).build();
     }
 
     public void updateStudent(Student student){
-
+        studentRepository4.updateStudentDetails(student);
     }
 
     public void deleteStudent(int id){
-        //Delete student and deactivate corresponding card
+        studentRepository4.deleteCustom(id);
+        cardService4.deactivateCard(id);
     }
 }
